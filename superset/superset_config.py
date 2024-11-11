7b09734e2519b7cd25cc589a6ade06e4481af5d0
umerghani7 <170563766+umerghani7@users.noreply.github.com>
2024-11-10 19:22:00 -0500

    repo setup

---
+++ superset/superset_config.py
@@ -0,0 +1,14 @@
+FEATURE_FLAGS = {
+    "ENABLE_TEMPLATE_PROCESSING": True,
+}
+
+ENABLE_PROXY_FIX = True
+SECRET_KEY = "YOUR_OWN_RANDOM_GENERATED_STRING"
+
+PREFERRED_DATABASES = [
+    "Apache Druid",
+    "Google BigQuery",
+    "Snowflake",
+    "Google Sheets",
+    "PostgreSQL",
+]
